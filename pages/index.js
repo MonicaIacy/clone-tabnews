815b1cc26c0dd58eddba0531957cc8e934902956
Mônica Mendes <monicamendes81@yahoo.com>
2023-11-12 13:44:19 +0000

    atualiza a frase da página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Dani, eu te amo e admiro muito!</h1>;
+  return (
+    <h1>
+      Um site em construção, feito para profissionais juniores na área da
+      tecnologia compartilharem seus desafios técnicos, comportamentais e
+      sociais, assim como suas soluções!
+    </h1>
+  );
 }
 
 export default Home;
