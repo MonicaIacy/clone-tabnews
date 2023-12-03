9cd9edef056d2272c19052187aee2f26a24f207a
Mônica Mendes <monicamendes81@yahoo.com>
2023-12-03 15:19:26 +0000

    add database info to `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "isso está em utf-8" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = (await database.query("SELECT version()")).rows[0]
+    .version;
+  const maxConnections = (await database.query("SHOW MAX_CONNECTIONS")).rows[0]
+    .max_connections;
+  const usedConnections = (
+    await database.query("SELECT sum(numbackends) FROM pg_stat_database;")
+  ).rows[0].sum;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      postgres_version: postgresVersion,
+      max_connections: Number(maxConnections),
+      used_connections: Number(usedConnections),
+    },
+  });
 }
 
 export default status;
